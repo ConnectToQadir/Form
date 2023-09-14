411ddde9a3142bc56007dd76d07d79c659f8bb00
Ghulam Qadir <connecttoqadir@gmail.com>
2023-09-14 18:40:01 +0500

    public done

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const mongoose = require('mongoose')
 const app = express()
 
 
-app.set("view engine",'ejs')
+
+app.use(express.static("public"))
 
 app.use('/api/students',require('./routes/students'))
 
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
 
-app.get("/some",(req,res)=>{
-    res.render("form")
-})
 
 
 mongoose.connect("mongodb+srv://qadir:qadir@edify.0i5koc5.mongodb.net/form?retryWrites=true&w=majority").then(()=>{
