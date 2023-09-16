836bd6622e48427a75beb0043b126490a9d770d7
Ghulam Qadir <connecttoqadir@gmail.com>
2023-09-16 16:35:39 +0500

    done

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const app = express()
 
 
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static("public"))
 
-app.use('/api/students',require('./routes/students'))
+app.use('/api/students', require('./routes/students'))
 
 
 
-app.get("/api/public",(req,res)=>{
+app.get("/api/public", (req, res) => {
     res.render("some")
 })
 
+app.get('/', function (req, res) {
+    res.render(__dirname + '/views/some.ejs');
+});
 
 
-mongoose.connect("mongodb+srv://qadir:qadir@edify.0i5koc5.mongodb.net/form?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://qadir:qadir@edify.0i5koc5.mongodb.net/form?retryWrites=true&w=majority").then(() => {
     console.log("Connected!")
 })
 
 
 var port = process.env.port || 4500
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Running...")
 })
